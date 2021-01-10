496588dc120be6a2b01506ac486102aad51eb7f2
cpmolnar <cpmolnar@gmail.com>
2021-01-09 19:25:48 -0600

    Cleanup and nice-ifying

--- model.py
+++ model.py
@@ -10,57 +10,92 @@ from keras.layers import Bidirectional, GlobalMaxPool1D
 from keras.layers import Convolution1D
 from keras.models import Model, Sequential
 
-def lstm_model(df, epochs, hps):
-    (max_features, maxlen, embedding_vector_length, lstm_units, dropout_val) = hps
-    tokenizer = Tokenizer(num_words=max_features)
-    tokenizer.fit_on_texts(df['Stem'])
-    list_tokenized_train = tokenizer.texts_to_sequences(df['Stem'])
+import matplotlib.pyplot as plt
 
-    X_train = pad_sequences(list_tokenized_train, maxlen=maxlen)
-    y_train = df['TotalPValue']
-    model = Sequential() 
-    model.add(Embedding(max_features, embedding_vector_length, input_length=maxlen))
-    model.add(Bidirectional(LSTM(lstm_units, return_sequences=True)))
-    model.add(Dropout(dropout_val))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='mse', optimizer=sgd, metrics=['mean_absolute_error', 'mean_absolute_percentage_error', 'mean_squared_logarithmic_error', 'poisson'])
-    # print(model.summary())
-    batch_size = 100
-    history = model.fit(X_train,y_train, batch_size=batch_size, epochs=epochs, validation_split=0.5, verbose=0)
-    return history
+class lstm_model:
+    def __init__(self, df, hps=(256, 32, 4, 1, 0.0), verbose = False):
+        self.df = df
+        self.epochs = 20
+        self.batch_size = 100
+        (self.max_features,
+         self.maxlen,
+         self.embedding_vector_length,
+         self.lstm_units,
+         self.dropout_val) = hps
+        self.tokenizer = Tokenizer(num_words=self.max_features)
+        self.tokenizer.fit_on_texts(self.df['Stem'])
+        self.verbose = verbose
 
-def tune_hyperparameters(df, epochs):
-    # Hyperparameter tuning
-    max_features_list = [512]
-    maxlen_list = [64]
-    embedding_vector_length_list = [4]
-    lstm_units_list = [1]
-    dropout_val_list = [0.0]
-    batch_size = 100
+    def train(self):
+        list_tokenized_train = self.tokenizer.texts_to_sequences(self.df['Stem'])
 
-    total_iterations = len(max_features_list) * len(maxlen_list) * len(embedding_vector_length_list) * len(lstm_units_list) * len(dropout_val_list)
+        X_train = pad_sequences(list_tokenized_train, maxlen=self.maxlen)
+        y_train = self.df['TotalPValue']
+        self.model = Sequential()
+        self.model.add(Embedding(self.max_features, self.embedding_vector_length, input_length=self.maxlen))
+        self.model.add(Bidirectional(LSTM(self.lstm_units, return_sequences=True)))
+        self.model.add(Dropout(self.dropout_val))
+        self.model.add(Flatten())
+        self.model.add(Dense(1, activation='sigmoid'))
+        sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(loss='mse', optimizer=sgd, metrics=['mean_absolute_error', 'mean_absolute_percentage_error', 'mean_squared_logarithmic_error', 'poisson'])
+        
+        self.history = self.model.fit(X_train,y_train, batch_size=self.batch_size, epochs=self.epochs, validation_split=0.5, verbose=0)
 
-    best_loss = 1
-    for max_features in max_features_list:
-        for maxlen in maxlen_list:
-            for embedding_vector_length in embedding_vector_length_list:
-                for lstm_units in lstm_units_list:
-                    for dropout_val in dropout_val_list:
-                        history = lstm_model(df, epochs, (max_features, maxlen, embedding_vector_length, lstm_units, dropout_val))
+        loss = (self.history.history['val_loss'][self.epochs-1])
+        return loss
+
+    def predict(self, input):
+        list_tokenized_input = self.tokenizer.texts_to_sequences(input.input)
+        padded_seq = pad_sequences(list_tokenized_input, maxlen=self.maxlen)
+        prediction = self.model.predict(padded_seq)
+        return padded_seq, prediction
+
+    def tune_hyperparameters(self):
+        # Hyperparameter tuning
+        max_features_list = [512]
+        maxlen_list = [64]
+        embedding_vector_length_list = [4]
+        lstm_units_list = [1]
+        dropout_val_list = [0.0]
+
+        total_iterations = len(max_features_list) * len(maxlen_list) * len(embedding_vector_length_list) * len(lstm_units_list) * len(dropout_val_list)
+
+        best_loss = 1
+        for max_features in max_features_list:
+            for maxlen in maxlen_list:
+                for embedding_vector_length in embedding_vector_length_list:
+                    for lstm_units in lstm_units_list:
+                        for dropout_val in dropout_val_list:
+                            sample_hps = (max_features, maxlen, embedding_vector_length, lstm_units, dropout_val)
+                            sample_model = lstm_model(self.df, sample_hps)
+                            sample_model.train()
                     
-                        loss = numpy.amin(history.history['val_loss'][epochs-1])
-                        if (loss < best_loss):
-                            best_loss = loss
-                            best_maxlen = maxlen
-                            best_max_features = max_features
-                            best_embedding_vector_length = embedding_vector_length
-                            best_lstm_units = lstm_units
-                            best_dropout_val = dropout_val
-                        pos = 1 + (max_features_list.index(max_features) * len(maxlen_list) * len(embedding_vector_length_list) * len(lstm_units_list) * len(dropout_val_list)) + (maxlen_list.index(maxlen) * len(embedding_vector_length_list) * len(lstm_units_list) * len(dropout_val_list)) + (embedding_vector_length_list.index(embedding_vector_length) * len(lstm_units_list) * len(dropout_val_list)) + (lstm_units_list.index(lstm_units) * len(dropout_val_list)) + dropout_val_list.index(dropout_val)
-                        print('\nIteration: %d / %d \tval_loss: \t%f \tmax_features: \t\t%d \tmaxlen: \t\t%d \tembedding_vector_length: \t%d \tlstm_units: \t\t%d \tdropout_val: \t\t%f' % (pos, total_iterations, loss, max_features, maxlen, embedding_vector_length, lstm_units, dropout_val))
-                        print('\t\t\tBest val_loss: \t%f \tBest max_features: \t%d \tBest maxlen: \t%d \tBest embedding_vector_length: \t%d \tBest lstm_units: \t%d \tBest dropout_val: \t%f' % (best_loss, best_max_features, best_maxlen, best_embedding_vector_length, best_lstm_units, best_dropout_val))
-                        keras.backend.clear_session()
+                            loss = numpy.amin(sample_model.history.history['val_loss'][self.epochs-1])
+                            if (loss < best_loss):
+                                best_loss = loss
+                                self.maxlen = maxlen
+                                self.max_features = max_features
+                                self.embedding_vector_length = embedding_vector_length
+                                self.lstm_units = lstm_units
+                                self.dropout_val = dropout_val
+                            pos = 1 + (max_features_list.index(max_features) * len(maxlen_list) * len(embedding_vector_length_list) * len(lstm_units_list) * len(dropout_val_list)) + (maxlen_list.index(maxlen) * len(embedding_vector_length_list) * len(lstm_units_list) * len(dropout_val_list)) + (embedding_vector_length_list.index(embedding_vector_length) * len(lstm_units_list) * len(dropout_val_list)) + (lstm_units_list.index(lstm_units) * len(dropout_val_list)) + dropout_val_list.index(dropout_val)
+                            print('\nIteration: %d / %d \nval_loss: \t%f \nmax_features: \t\t%d \nmaxlen: \t\t%d \nembedding_vector_length: \t%d \nlstm_units: \t\t%d \ndropout_val: \t\t%f' % (pos, total_iterations, loss, max_features, maxlen, embedding_vector_length, lstm_units, dropout_val))
+                            keras.backend.clear_session()
 
-    return best_loss, (best_maxlen, best_max_features, best_embedding_vector_length, best_lstm_units, best_dropout_val)
+        if (self.verbose):
+            print('\t\t\tBest val_loss: \t%f \nBest max_features: \t%d \nBest maxlen: \t%d \nBest embedding_vector_length: \t%d \nBest lstm_units: \t%d \nBest dropout_val: \t%f' % (best_loss, self.max_features, self.maxlen, self.embedding_vector_length, self.lstm_units, self.dropout_val))
+        return best_loss
+
+    def plot_loss(self):
+        # Plot the data
+        history_dict = self.history.history
+        loss_values = history_dict['loss']
+        val_loss_values = history_dict['val_loss']
+        plt.plot(range(self.epochs), loss_values, 'bo', label='Training loss')
+        plt.plot(range(self.epochs), val_loss_values, 'b', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
--- nlp_keras.py
+++ nlp_keras.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy
 from numpy import array
-import matplotlib.pyplot as plt
 
 import en_core_web_sm
 
-import model
-from utils import clean_text, remove_tokens_on_match
+from model import lstm_model
+from nlp_utils import clean_text, remove_tokens_on_match
 
 # Load question data and their p-values
 print("Loading the data...")
-df = pd.read_csv('models/results.csv', delimiter=",").sample(frac=1)
+df = pd.read_csv('models/results-not-deleted-RAD.csv', delimiter=",").sample(frac=1)
 
 print("Cleaning the text...")
 df['Stem'] = df.Stem.apply(clean_text)
@@ -29,38 +28,26 @@ print(df.head(30))
 # print("Average number of words per question: %s" % df.Stem.apply(lambda x: len(x.split(" "))).mean())
 
 # LSTM setup with best hyperparameters
-epochs = 20
-loss, hps = model.tune_hyperparameters(df, epochs)
-history = model.lstm_model(df, epochs, hps)
-loss = (history.history['val_loss'][epochs-1])
+model = lstm_model(df)
+print("Tuning hyperparameters...")
+best_loss = model.tune_hyperparameters()
+print('\nBest loss: %f' % (best_loss))
+loss = model.train()
 print('Actual loss: %f' % (loss))
-    
-# Plot the data
-history_dict = history.history
-loss_values = history_dict['loss']
-val_loss_values = history_dict['val_loss']
-epochs = range(1, len(history_dict['loss']) + 1)
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
-plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
+model.plot_loss()
 
 # Read inputs for testing
-pd.set_option('display.max_rows', hps[0])
+pd.set_option('display.max_rows', model.max_features)
 while 1:
+    print("\nInput a new question to estimate its difficulty:")
     x_in = input()
     tmp = []
     tmp.append(x_in)
     df_input = pd.DataFrame(tmp, columns={'input'})
-    df_input = df_input.input.apply(lambda x: clean_text(x))
-    print("Cleaned input: %s" % df_input)
+    #df_input = df_input.input.apply(lambda x: clean_text(x))
     #df_input = df_input.input.apply(remove_tokens_on_match)
     #print("POS removed input: %s" % df_input)
-    list_tokenized_input = tokenizer.texts_to_sequences(df_input)
-    X_te = pad_sequences(list_tokenized_input, maxlen=hps[0])
-    prediction = model.predict(X_te)
-    print("Padded, tokenized input: %s" % X_te)
+    print("Cleaned input: %s" % df_input.input)
+    padded_seq, prediction = model.predict(df_input)
+    print("Padded, tokenized input: %s" % padded_seq)
     print("Prediction: %.4f" % prediction)
